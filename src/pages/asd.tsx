c736f140e3625a6cb67013f03aaefb07cf0400ec
Pratik Sangami <9841pratik@gmail.com>
2024-01-25 13:54:02 +0545

    Add new pagrse

---
+++ src/pages/asd.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const About = () => {
+  return (
+    <main>
+        About page
+    </main>
+  )
+}
+
+export const Head = () => <title>Asd Page</title>
+
+
+export default About
